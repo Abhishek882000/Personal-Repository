1feef3a06b076f0cf0a4023f386cc0891ae05259
Abhishek Kumar Jha <jhaabhishek318@gmail.com>
2022-12-06 14:29:39 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -16,12 +16,35 @@
 
 // QuerySelector && QuerySelectorALL
 
-const queryAll = document.querySelectorAll(".list-group-item");
-for (let i in queryAll) {
-    if (i % 2 == 0) {
-        queryAll[i].style.backgroundColor = "green";
-        queryAll[i].style.color = "white";
-    }
-}
-queryAll[1].style.color = "green";
-console.log(queryAll[1]);
+// const queryAll = document.querySelectorAll(".list-group-item");
+// for (let i in queryAll) {
+//     if (i % 2 == 0) {
+//         queryAll[i].style.backgroundColor = "green";
+//         queryAll[i].style.color = "white";
+//     }
+// }
+// queryAll[1].style.color = "green";
+// console.log(queryAll[1]);
+
+
+// Creating Nodes and Modifying Dom
+
+const listGroup = document.getElementById("items");
+console.log(listGroup.parentElement.parentElement);
+console.log(listGroup.lastElementChild);
+console.log(listGroup.lastChild);
+console.log(listGroup.childNodes);
+console.log(listGroup.children[1]);
+console.log(listGroup.firstChild);
+console.log(listGroup.firstElementChild);
+console.log(listGroup.firstElementChild.textContent);
+console.log(listGroup.nextSibling);
+console.log(listGroup.nextElementSibling);
+console.log(listGroup.previousSibling);
+
+let newElement = document.createElement('div');
+newElement.className = "hello";
+let newDivText = document.createTextNode("Hello");
+newElement.appendChild(newDivText);
+newElement.setAttribute("title", "newTitle");
+console.log(newElement);
