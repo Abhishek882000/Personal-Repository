b5ba00cd2dd23d7211abf9f63960263049676e76
Abhishek Kumar Jha <jhaabhishek318@gmail.com>
2022-12-06 13:39:48 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -1,5 +1,14 @@
-const listElement = document.getElementById("listItem3");
-const listGroup = document.getElementById("items");
+// const listElement = document.getElementById("listItem3");
+// const listGroup = document.getElementById("items");
 
-listElement.style.backgroundColor = "green";
-listGroup.style.fontWeight = "bold";
+// listElement.style.backgroundColor = "green";
+// listGroup.style.fontWeight = "bold";
+
+// Task GetElementsbyTAgName
+
+let node = document.createElement('li');
+node.appendChild(document.createTextNode('Item 5'));
+document.querySelector('.list-group-item:nth-child(4)').appendChild(node);
+const collection = document.getElementsByTagName("li");
+document.getElementById("listItem3").innerHTML = collection[4].innerHTML;
+const listElem = document.getElementsByClassName("list-group-item");
