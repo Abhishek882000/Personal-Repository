6ad7726c5b37550e69ce09446e2e538bb2794261
Abhishek Kumar Jha <jhaabhishek318@gmail.com>
2022-12-06 16:08:19 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -51,55 +51,82 @@
 
 
 
-// Delete and Edit functionality
+// // Delete and Edit functionality
+
+// var form = document.getElementById('addForm');
+// var itemList = document.getElementById('items');
+// var filter = document.getElementById('filter');
+
+// // Form submit event
+// form.addEventListener('submit', addItem);
+// // Delete event
+// itemList.addEventListener('click', removeItem);
+// // Filter event
+// filter.addEventListener('keyup', filterItems);
+
+// // Add item
+// function addItem(e) {
+//     e.preventDefault();
+
+//     var newItem = document.getElementById('item').value;
+
+//     var li = document.createElement('li');
+//     li.className = 'list-group-item';
+
+//     li.appendChild(document.createTextNode(newItem));
+
+
+//     var deleteBtn = document.createElement('button');
+
+
+//     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+
+//     deleteBtn.appendChild(document.createTextNode('X'));
+
+
+//     li.appendChild(deleteBtn);
+
+
+//     itemList.appendChild(li);
+// }
+
+
+// function removeItem(e) {
+//     if (e.target.classList.contains('delete')) {
+//         if (confirm('Are You Sure?')) {
+//             var li = e.target.parentElement;
+//             itemList.removeChild(li);
+//         }
+//     }
+// }
+
+
+// function filterItems(e) {
+
+//     var text = e.target.value.toLowerCase();
+
+//     var items = itemList.getElementsByTagName('li');
+
+//     Array.from(items).forEach(function (item) {
+//         var itemName = item.firstChild.textContent;
+//         if (itemName.toLowerCase().indexOf(text) != -1) {
+//             item.style.display = 'block';
+//         } else {
+//             item.style.display = 'none';
+//         }
+//     });
+// }
+
+
+
+//Filter functionality
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
 filter.addEventListener('keyup', filterItems);
-
-// Add item
-function addItem(e) {
-    e.preventDefault();
-
-    var newItem = document.getElementById('item').value;
-
-    var li = document.createElement('li');
-    li.className = 'list-group-item';
-
-    li.appendChild(document.createTextNode(newItem));
-
-
-    var deleteBtn = document.createElement('button');
-
-
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-
-    deleteBtn.appendChild(document.createTextNode('X'));
-
-
-    li.appendChild(deleteBtn);
-
-
-    itemList.appendChild(li);
-}
-
-
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        if (confirm('Are You Sure?')) {
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
-    }
-}
 
 
 function filterItems(e) {
