595813f19c37c806641d5594db4a6782d9be9d5f
Abhishek Kumar Jha <jhaabhishek318@gmail.com>
2022-12-06 14:13:33 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -6,9 +6,22 @@
 
 // Task GetElementsbyTAgName
 
-let node = document.createElement('li');
-node.appendChild(document.createTextNode('Item 5'));
-document.querySelector('.list-group-item:nth-child(4)').appendChild(node);
-const collection = document.getElementsByTagName("li");
-document.getElementById("listItem3").innerHTML = collection[4].innerHTML;
-const listElem = document.getElementsByClassName("list-group-item");
+// let node = document.createElement('li');
+// node.appendChild(document.createTextNode('Item 5'));
+// document.querySelector('.list-group-item:nth-child(4)').appendChild(node);
+// const collection = document.getElementsByTagName("li");
+// document.getElementById("listItem3").innerHTML = collection[4].innerHTML;
+// const listElem = document.getElementsByClassName("list-group-item");
+
+
+// QuerySelector && QuerySelectorALL
+
+const queryAll = document.querySelectorAll(".list-group-item");
+for (let i in queryAll) {
+    if (i % 2 == 0) {
+        queryAll[i].style.backgroundColor = "green";
+        queryAll[i].style.color = "white";
+    }
+}
+queryAll[1].style.color = "green";
+console.log(queryAll[1]);
